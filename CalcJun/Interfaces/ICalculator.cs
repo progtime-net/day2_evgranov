c87e752f8ff68d3ded05801de5efe8db6295385a
Ivan Filipenkov <hello@vanutp.dev>
2023-08-12 17:14:47 +0300

    Interfaces

---
+++ CalcJun/Interfaces/ICalculator.cs
@@ -0,0 +1,8 @@
+﻿namespace CalcJun.Interfaces
+{
+    interface ICalculator
+    {
+        void InputAndCalculate();
+        float ExecuteOperation(string operation, float a, float b);
+    }
+}
---
+++ CalcJun/Interfaces/IOperation.cs
@@ -0,0 +1,8 @@
+﻿namespace CalcJun.Interfaces
+{
+    interface IOperation
+    {
+        string Name { get; }
+        float Execute(float a, float b);
+    }
+}
